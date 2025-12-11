2f1b7d33a13407d9793764785fbfa20467297a73
Dongxu Chen <1625326705@qq.com>
2025-12-11 20:33:35 +0800

    update scripts

--- scripts/nav.py
+++ scripts/nav.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--command", type=str, required=True, help="command to execute")
     parser.add_argument("-id", "--cat_id", type=str, help="category id for navigation")
+    parser.add_argument("-tid", "--task_id", type=str, help="task id for state query")
     args = parser.parse_args()
 
     if args.command == "start":
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     elif args.command == "stop":
         os.system(f"curl '0.0.0.0:8080/api/navigation/stop'")
     elif args.command == "state":
-        os.system(f"curl '0.0.0.0:8080/api/navigation/state'")
+        os.system(f"curl '0.0.0.0:8080/api/navigation/state?task_id={args.task_id}'")
     else:
         print(f"Unknown command: {args.command}")
 
